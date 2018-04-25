252c27ecb80bbae7bc31e68fcbbaa3fea5587911
Mustafa Al-Bassam <mus@musalbas.com>
2018-04-25 15:22:23 +0100

    Update channel.sol

--- channel.sol
+++ channel.sol
@@ -15,7 +15,7 @@ contract Channel {
 		channelTimeout = timeout;
 	}
 
-	function CloseChannel(bytes32 h, uint8 v, bytes32 r, bytes32 s, uint value){
+	function CloseChannel(bytes32 h, uint8 v, bytes32 r, bytes32 s, uint value, bytes32 hashedkey, bytes32 key){
 
 		address signer;
 		bytes32 proof;
@@ -26,7 +26,10 @@ contract Channel {
 		// signature is invalid, throw
 		if (signer != channelSender && signer != channelRecipient) throw;
 
-		proof = sha3(this, value);
+		proof = sha3(this, value, hashedkey);
+
+		// if the signer is the recipient, they should provide the key
+		if (signer == channelRecipient && sha3(key) != hashedkey) throw;
 
 		// signature is valid but doesn't match the data provided
 		if (proof != h) throw;
